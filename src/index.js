810cde77980b93c238521befdc3c3727691564f3
Mikhailyuk Sergey <vzxyqw@gmail.com>
2020-12-06 22:24:33 +0200

    done work

--- src/index.js
+++ src/index.js
@@ -9,23 +9,29 @@ class CountdownTimer {
     this.selector = obj.selector;
     this.targetDate = obj.targetDate;
   }
+
+  start() {
+    setInterval(() => {
+      const time = this.targetDate.getTime() - Date.now();
+      this.updateClockface(time);
+    }, 1000);
+  }
+
+  updateClockface(time) {
+    const days = Math.floor(time / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const mins = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
+    const secs = Math.floor((time % (1000 * 60)) / 1000);
+    daysRef.textContent = String(days).padStart(2, '0');
+    hoursRef.textContent = String(hours).padStart(2, '0');
+    minsRef.textContent = String(mins).padStart(2, '0');
+    secsRef.textContent = String(secs).padStart(2, '0');
+  }
 }
 
 const timer = new CountdownTimer({
   selector: '#timer-1',
   targetDate: new Date('September 18, 2021'),
 });
-const timerId = setInterval(makeTimer, 1000);
 
-function makeTimer() {
-  const time = timer.targetDate.getTime() - Date.now();
-  const days = Math.floor(time / (1000 * 60 * 60 * 24));
-  const hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  const mins = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
-  const secs = Math.floor((time % (1000 * 60)) / 1000);
-  daysRef.textContent = String(days).padStart(2, '0');
-  hoursRef.textContent = String(hours).padStart(2, '0');
-  minsRef.textContent = String(mins).padStart(2, '0');
-  secsRef.textContent = String(secs).padStart(2, '0');
-}
-makeTimer();
+timer.start();
